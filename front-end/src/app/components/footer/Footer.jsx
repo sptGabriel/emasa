0b8149bc54ac6700019606b9bc756ca128842190
gabriel <gabrielcerqueira96@hotmail.com>
2020-03-10 08:18:25 -0300

    working on footer

--- front-end/src/app/components/footer/Footer.jsx
+++ front-end/src/app/components/footer/Footer.jsx
@@ -1,11 +1,35 @@
 import React from 'react';
-import { FooterTop, FooterBottom } from './styles/footer';
+import { FooterTop, FooterBottom, Container, Content } from './styles/footer';
 import { Footer } from 'app/globals/global_styles';
 const FooterMain = props => {
   return (
     <Footer>
-      <FooterTop>a</FooterTop>
-      <FooterBottom>b</FooterBottom>
+      <FooterTop>
+        <Container>
+          <Content>
+            <div className="mapa">
+              <h3 className="title">Mapa do Site</h3>
+              <ul className="nav_footer">
+                <li>A Empresa</li>
+                <li>Fale Conosco</li>
+                <li>Licitações</li>
+                <li>Meio Ambiente</li>
+                <li>Transparência</li>
+                <li>Loja Virtual</li>
+                <li>Niveis dos Rios</li>
+                <li>Notícias</li>
+                <li>Sanemaento</li>
+                <li>Social</li>
+                <li>WebMail</li>
+                <li>Login</li>
+              </ul>
+            </div>
+          </Content>
+        </Container>
+      </FooterTop>
+      <FooterBottom>
+        <Container>a</Container>
+      </FooterBottom>
     </Footer>
   );
 };
--- front-end/src/app/components/footer/styles/footer.js
+++ front-end/src/app/components/footer/styles/footer.js
@@ -3,11 +3,50 @@ import { mxw80 } from 'app/globals/global_styles';
 export const FooterTop = styled.div`
   width: 100%;
   height: auto !important;
+  padding: 30px 0;
   background: #05172d;
+  & .title {
+    width: 100%;
+    color: white;
+    font-family: 'Poppins', sans-serif;
+    padding-bottom: 10px;
+    font-size: 700;
+  }
+  & .mapa {
+    width: 50%;
+    display: flex;
+    flex-direction: column;
+    background: red;
+  }
+  & .nav_footer {
+    width: 100%;
+    display: flex;
+    padding: 10px 0;
+    background: hotpink;
+    color: white;
+    font-family: sans-serif;
+    flex-wrap: wrap;
+    li {
+      margin-right: 100px;
+      padding: 10px 0;
+      background: red;
+    }
+  }
 `;
 
 export const FooterBottom = styled.div`
   width: 100%;
   height: auto !important;
   background: #004fff;
+`;
+export const Container = styled.div`
+  ${mxw80}
+  position:relative;
+  height: auto;
+`;
+
+export const Content = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
 `;
