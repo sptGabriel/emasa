3e8bb3f19659d2746d4b2241675bb0728cb6081b
root <habboogro@gmail.com>
2020-03-23 23:13:36 -0400

    lol

--- back-end/src/entity/User.ts
+++ back-end/src/entity/User.ts
@@ -7,20 +7,28 @@ import {
   BaseEntity
 } from "typeorm";
 
+import { ObjectType, Field, ID, Root } from "type-graphql";
+
+@ObjectType()
 @Entity()
 export class User extends BaseEntity {
+  @Field(() => ID)
   @PrimaryGeneratedColumn()
   id: number;
 
+  @Field()
   @Column()
   login: string;
 
+  @Field()
   @Column()
   password: string;
 
+  @Field()
   @CreateDateColumn()
   created: Date;
 
+  @Field()
   @UpdateDateColumn()
   updated: Date;
 }
--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -2,27 +2,28 @@ import "reflect-metadata";
 import { createConnection } from "typeorm";
 import express from "express";
 import { ApolloServer } from "apollo-server-express";
-
+import cors from "cors";
 import schema from "./schemas/index";
 
-const startServer = async () => {
+const main = async () => {
+  await createConnection();
   const server = new ApolloServer({
     schema,
     context: ({ req, res }: any) => ({ req, res })
   });
-  let retries = 5;
-  while (retries) {
-    try {
-      await createConnection();
-      break;
-    } catch (err) {
-      console.log(err);
-      retries -= 1;
-      console.log(`retries left: ${retries}`);
+  // let retries = 5;
+  // while (retries) {
+  //   try {
+  //     await createConnection();
+  //     break;
+  //   } catch (err) {
+  //     console.log(err);
+  //     retries -= 1;
+  //     console.log(`retries left: ${retries}`);
 
-      await new Promise(res => setTimeout(res, 5000));
-    }
-  }
+  //     await new Promise(res => setTimeout(res, 5000));
+  //   }
+  // }
   const app = express();
 
   server.applyMiddleware({ app });
@@ -32,4 +33,4 @@ const startServer = async () => {
   );
 };
 
-startServer();
+main();
